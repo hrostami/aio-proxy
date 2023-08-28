22b3ad9522ca606e904a416ce5077d9c3a47113b
Hosy <rostamimofrad@gmail.com>
2023-08-28 17:50:35 +0330

    better menu

--- setup_menu.sh
+++ setup_menu.sh
@@ -2,20 +2,32 @@
 
 # Function to display the menu
 display_menu() {
-    echo -e "\033[1;32mHysteria and Tuic Setup Menu\033[0m"
+    clear
+    echo "**********************************************"
+    echo -e "\033[1;32m     Hysteria and Tuic Setup Menu\033[0m"
+    echo "**********************************************"
     echo "1. Run Hysteria Setup"
     echo "2. Run Tuic Setup"
     echo "0. Exit"
+    echo "**********************************************"
 }
 
 # Function to run Hysteria setup script
 run_hysteria_setup() {
+    clear
+    echo "Running Hysteria Setup..."
+    sleep 2
     bash hysteria_setup_script.sh
+    read -p "Press Enter to continue..."
 }
 
 # Function to run Tuic setup script
 run_tuic_setup() {
+    clear
+    echo "Running Tuic Setup..."
+    sleep 2
     bash tuic_setup_script.sh
+    read -p "Press Enter to continue..."
 }
 
 while true; do
@@ -25,7 +37,7 @@ while true; do
     case $choice in
         1) run_hysteria_setup ;;
         2) run_tuic_setup ;;
-        0) echo "Exiting..."; exit ;;
+        0) clear; echo "Exiting..."; exit ;;
         *) echo "Invalid choice. Please select a valid option." ;;
     esac
 done

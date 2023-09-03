b0cbbc47e21b1fbdb2c57e472148bc670c577a85
Hosy <rostamimofrad@gmail.com>
2023-09-04 01:14:03 +0330

    added delete option

--- setup_menu.sh
+++ setup_menu.sh
@@ -28,11 +28,14 @@ fi
 display_menu() {
     clear
     echo "**********************************************"
-    echo -e "\033[1;32m     Hysteria and Tuic Setup Menu\033[0m"
+    echo -e "\033[1;32m     Proxy Setup Menu\033[0m"
     echo "**********************************************"
     echo "1. Run Hysteria Setup"
     echo "2. Run Hysteria v2 Setup"
     echo "3. Run Tuic Setup"
+    echo "4. Delete Hysteria Proxy"
+    echo "5. Delete Hysteria v2 Proxy"
+    echo "6. Delete Tuic Proxy"
     echo "0. Exit"
     echo "**********************************************"
 }
@@ -64,6 +67,39 @@ run_tuic_setup() {
     read -p "Press Enter to continue..."
 }
 
+# Function to delete Hysteria Proxy
+delete_hysteria_proxy() {
+    clear
+    echo "Deleting Hysteria Proxy..."
+    sleep 2
+    rm -r ../hy
+    systemctl stop hy
+    systemctl disable hy
+    read -p "Press Enter to continue..."
+}
+
+# Function to delete Hysteria v2 Proxy
+delete_hysteria_v2_proxy() {
+    clear
+    echo "Deleting Hysteria v2 Proxy..."
+    sleep 2
+    rm -r ../hy2
+    systemctl stop hy2
+    systemctl disable hy2
+    read -p "Press Enter to continue..."
+}
+
+# Function to delete Tuic Proxy
+delete_tuic_proxy() {
+    clear
+    echo "Deleting Tuic Proxy..."
+    sleep 2
+    rm -r ../tuic
+    systemctl stop tuic
+    systemctl disable tuic
+    read -p "Press Enter to continue..."
+}
+
 while true; do
     display_menu
     read -p "Enter your choice: " choice
@@ -72,6 +108,9 @@ while true; do
         1) run_hysteria_setup ;;
         2) run_hysteria_v2_setup ;;
         3) run_tuic_setup ;;
+        4) delete_hysteria_proxy ;;
+        5) delete_hysteria_v2_proxy ;;
+        6) delete_tuic_proxy ;;
         0) clear; echo "Exiting..."; exit ;;
         *) echo "Invalid choice. Please select a valid option." ;;
     esac

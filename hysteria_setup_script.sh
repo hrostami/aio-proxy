2ac5ab53839f802899b10b11e2154f1da6518a1c
Hosy <rostamimofrad@gmail.com>
2023-09-10 19:49:06 +0330

    hysteria back to def

--- hysteria_setup_script.sh
+++ hysteria_setup_script.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+# Update the system
 apt-get update 
+
+# Install necessary packages
 apt-get install wget nano -y
+
+# Install net-tools
 apt-get install net-tools -y
 
+# Detect user and set the directory path
 if [ "$EUID" -eq 0 ]; then
     user_directory="/root/hy"
 else
     user_directory="/home/$USER/hy"
 fi
 
+# Check if Hysteria directory exists
 if [ -d "$user_directory" ]; then
     clear
     echo "--------------------------------------------------------------------------------"
     echo -e "\e[1;33mHysteria directory already exists. Checking for latest version..\e[0m"
     echo "--------------------------------------------------------------------------------"
     sleep 2
-
+    # Check if the config.json file exists
     if [ -f "$user_directory/config.json" ]; then
+        # Read the port and obfuscation password from config.json
         port=$(jq -r '.listen' "$user_directory/config.json" | cut -c 2-)
         password=$(jq -r '.obfs' "$user_directory/config.json")
     else
@@ -25,11 +33,14 @@ if [ -d "$user_directory" ]; then
         return
     fi
 else
+    # Prompt user for port and password
     read -p "Enter the listening port: " port
     read -p "Enter the obfuscation password: " password
 
+    # Create the directory
     mkdir -p "$user_directory"
 
+    # Create the hysteria configuration file
     cat << EOF > config.json
     {
     "listen": ":$port",
@@ -44,12 +55,14 @@ else
 EOF
 fi
 
+# Detect the latest version of the GitHub repository
 # latest_version=$(curl -s https://api.github.com/repos/apernet/hysteria/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 latest_version="v1.3.5"
 echo -e "\e[1;33m---> Installing hysteria ver $latest_version\e[0m"
 echo "--------------------------------------------------------------------------------"
 sleep 2
 
+# Detect architecture and download the appropriate file
 architecture=$(uname -m)
 if [ "$architecture" = "x86_64" ]; then
     wget "https://github.com/apernet/hysteria/releases/download/$latest_version/hysteria-linux-amd64"
@@ -58,6 +71,7 @@ else
     mv hysteria-linux-arm hysteria-linux-amd64
 fi
 
+# Provide execute permissions to the downloaded file
 chmod 755 hysteria-linux-amd64
 
 # Generate encryption keys if they don't exist
@@ -87,15 +101,16 @@ if [ ! -f "/etc/systemd/system/hy.service" ]; then
     WantedBy=multi-user.target
 EOF
 
+    # Reload systemd and enable the service
     systemctl daemon-reload
     systemctl enable hy
 fi
 
 
+# Restart the hysteria service and display its status
 systemctl restart hy
 
-# show configs
-
+# Get public IPs
 IPV4=$(curl -s https://v4.ident.me)
 if [ $? -ne 0 ]; then
     echo "Error: Failed to get IPv4 address"
@@ -108,9 +123,11 @@ if [ $? -ne 0 ]; then
     return
 fi
 
+# Create URLs
 IPV4_URL="hysteria://$IPV4:$port?protocol=udp&insecure=1&upmbps=100&downmbps=100&obfs=xplus&obfsParam=$password#hysteria"
 IPV6_URL="hysteria://[$IPV6]:$port?protocol=udp&insecure=1&upmbps=100&downmbps=100&obfs=xplus&obfsParam=$password#hysteria"
 
+# Print URLs
 echo "----------------config info-----------------"
 echo -e "\e[1;33mPassword: $password\e[0m"
 echo "--------------------------------------------"

fda4c45ccad8b7ae6136b943707f378752104053
hrostami <rostamimofrad@gmail.com>
2023-11-27 17:32:41 +0330

    menu

--- android-chisel.sh
+++ android-chisel.sh
@@ -85,6 +85,10 @@ get_user_input() {
     DOMAIN=${USER_DOMAIN:-$DOMAIN}
     echo -e "{\n\"SOCKS5_PORT\": \"$SOCKS5_PORT\", \n\"DOMAIN\": \"$DOMAIN\"\n}" > "$CONFIG_FILE"
 }
+
+echo
+white "---------------------------------------------"
+echo
 bblue "             █████╗ ██╗ ██████╗              "
 bblue "            ██╔══██╗██║██╔═══██╗             "
 bblue "            ███████║██║██║   ██║             "
@@ -94,10 +98,7 @@ bblue "            ╚═╝  ╚═╝╚═╝ ╚═════╝          
 bblue "           All-in-one Proxy Tool             "
 white "              Created by Hosy                "
 white "---------------------------------------------"
-white " Github: https://github.com/hrostami"
-white " Twitter: https://twitter.com/hosy000"
 echo
-echo -e "${plain}Thank you ${red}iSegaro${plain} for all your efforts! "
 
 get_user_input
 
